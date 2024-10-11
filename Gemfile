b763092a0c64f652cfbab0bc5d05a3cef7cbb4ab
Carlos A <cayala.w@gmail.com>
2024-10-11 11:36:31 -0500

    update script: generate PDF with images

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# frozen_string_literal
+
+source 'https://rubygems.org'
+
+gem 'csv'
--- lumni.rb
+++ lumni.rb
@@ -3,15 +3,15 @@
 require 'csv'
 
 directory_path = ARGV[0]
-directory_path = directory_path.match?(%r{/$}) ? directory_path : directory_path + '/'
-puts 'directory:', directory_path
-# files = Dir.glob(File.join(directory_path, '*.pdf'))
-# puts 'files type', files.class
-# puts files
+directory_path = directory_path.match?(%r{/$}) ? directory_path : "#{directory_path}/"
+# puts 'directory:', directory_path
+
 files = []
 Dir.entries(directory_path).each do |entry|
   next unless File.file?(File.join(directory_path, entry))
+  next unless entry.end_with?('.png')
 
+  # puts 'entry.class:', entry.class, ' entry: ', entry, ' extension-less entry: ', entry[0, entry.length - 4]
   date = File.ctime(File.join(directory_path, entry)).strftime('%Y-%m-%d')
   new_entry = entry[0, entry.length - 4]
   position = new_entry.split(' - ')[2..].join(' ')
@@ -19,7 +19,7 @@ Dir.entries(directory_path).each do |entry|
   platform = new_entry.split(' - ')[0]
   files.push({ original_name: entry, date: date, position: position, company: company, platform: platform })
 end
-puts files
+# puts files
 
 csv_file = File.join(directory_path, "postulaciones_#{directory_path.split('/').last}.csv")
 CSV.open(csv_file, 'w', headers: ['Fecha de postulación', 'Cargo', 'Empresa', 'Plataforma', 'Estado de proceso'],
@@ -30,3 +30,8 @@ CSV.open(csv_file, 'w', headers: ['Fecha de postulación', 'Cargo', 'Empresa', '
 end
 
 puts 'CSV file created:', csv_file
+
+# create file
+pdf_images = system("img2pdf #{directory_path}*.png -o \
+                    #{directory_path}postulaciones_#{directory_path.split('/').last}.pdf")
+puts "PDF created: #{pdf_images}"
