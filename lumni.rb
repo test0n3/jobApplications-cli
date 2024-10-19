8038da58db2a921a3cf7865240ea90d557d6019f
Carlos A <cayala.w@gmail.com>
2024-10-18 23:02:45 -0500

    process ordered files into PDF

--- lumni.rb
+++ lumni.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'shellwords'
 
 directory_path = ARGV[0]
 directory_path = directory_path.match?(%r{/$}) ? directory_path : "#{directory_path}/"
-# puts 'directory:', directory_path
+application_month = directory_path.split('/').last
 
 files = []
 Dir.entries(directory_path).each do |entry|
   next unless File.file?(File.join(directory_path, entry))
   next unless entry.end_with?('.png')
 
-  # puts 'entry.class:', entry.class, ' entry: ', entry, ' extension-less entry: ', entry[0, entry.length - 4]
-  date = File.ctime(File.join(directory_path, entry)).strftime('%Y-%m-%d')
-  new_entry = entry[0, entry.length - 4]
-  position = new_entry.split(' - ')[2..].join(' ')
-  company =  new_entry.split(' - ')[1]
-  platform = new_entry.split(' - ')[0]
+  date = File.birthtime(File.join(directory_path, entry)).strftime('%Y-%m-%d')
+  filename = File.basename(entry, File.extname(entry))
+  position = filename.split(' - ')[2..].join(' ')
+  company =  filename.split(' - ')[1]
+  platform = filename.split(' - ')[0]
   files.push({ original_name: entry, date: date, position: position, company: company, platform: platform })
 end
+
+files = files.sort_by { |file| file[:date] }
 # puts files
 
-csv_file = File.join(directory_path, "postulaciones_#{directory_path.split('/').last}.csv")
+csv_file = File.join(directory_path, "postulaciones_#{application_month}.csv")
 CSV.open(csv_file, 'w', headers: ['Fecha de postulación', 'Cargo', 'Empresa', 'Plataforma', 'Estado de proceso'],
                         write_headers: true) do |csv|
   files.each do |row|
@@ -31,7 +33,9 @@ end
 
 puts 'CSV file created:', csv_file
 
+files_to_process = files.map { |file| directory_path + Shellwords.escape(file[:original_name]) }.join(' ')
+# puts files_to_process
+
 # create file
-pdf_images = system("img2pdf #{directory_path}*.png -o \
-                    #{directory_path}postulaciones_#{directory_path.split('/').last}.pdf")
+pdf_images = system("img2pdf #{files_to_process} -o #{directory_path}postulaciones_#{application_month}.pdf")
 puts "PDF created: #{pdf_images}"
