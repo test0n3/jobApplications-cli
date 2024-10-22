36bde67a05c8a9bb8f4d3aa2aa004581b0c36dda
Carlos A <cayala.w@gmail.com>
2024-10-21 21:06:11 -0500

    cleaning code

--- lumni.rb
+++ lumni.rb
@@ -1,43 +1,49 @@
 # frozen_string_literal: true
 
 require 'csv'
-# require 'shellwords'
+
+def process_directory(directory_path)
+  files = []
+  Dir.entries(directory_path)
+     .select { |entry| entry.end_with?('.png') }
+     .sort_by { |entry| File.birthtime(File.join(directory_path, entry)) }
+     .each do |entry|
+    filename = File.basename(entry, File.extname(entry)).split(' - ')
+    files.push({ original_name: directory_path + entry,
+                 date: File.birthtime(File.join(directory_path, entry)).strftime('%Y-%m-%d'),
+                 position: filename[2..].join(' '),
+                 company: filename[1],
+                 platform: filename[0] })
+  end
+  files
+end
+
+def create_csv(directory_path, files)
+  application_month = directory_path.split('/').last
+  csv_file = File.join(directory_path, "postulaciones_#{application_month}.csv")
+  CSV.open(csv_file, 'w',
+           headers: ['Fecha de postulación', 'Cargo', 'Empresa', 'Plataforma', 'Estado de proceso'],
+           write_headers: true) do |csv|
+    files.each do |row|
+      csv << [row[:date], row[:position], row[:company], row[:platform], 'en proceso']
+    end
+  end
+
+  puts 'CSV file created:', csv_file
+end
+
+def create_pdf(directory_path, files)
+  application_month = directory_path.split('/').last
+  files_to_process = files.map { |file| file[:original_name] }
+  pdf_images = system('img2pdf', *files_to_process, '-o',
+                      "#{directory_path}postulaciones_#{application_month}.pdf")
+
+  puts "PDF created: #{pdf_images}"
+end
 
 directory_path = ARGV[0]
 directory_path = directory_path.match?(%r{/$}) ? directory_path : "#{directory_path}/"
-application_month = directory_path.split('/').last
-
-files = []
-Dir.entries(directory_path).each do |entry|
-  next unless File.file?(File.join(directory_path, entry))
-  next unless entry.end_with?('.png')
-
-  date = File.birthtime(File.join(directory_path, entry)).strftime('%Y-%m-%d')
-  filename = File.basename(entry, File.extname(entry))
-  position = filename.split(' - ')[2..].join(' ')
-  company =  filename.split(' - ')[1]
-  platform = filename.split(' - ')[0]
-  files.push({ original_name: entry, date: date, position: position, company: company, platform: platform })
-end
-
-files = files.sort_by { |file| file[:date] }
-
-csv_file = File.join(directory_path, "postulaciones_#{application_month}.csv")
-CSV.open(csv_file, 'w', headers: ['Fecha de postulación', 'Cargo', 'Empresa', 'Plataforma', 'Estado de proceso'],
-                        write_headers: true) do |csv|
-  files.each do |row|
-    csv << [row[:date], row[:position], row[:company], row[:platform], 'en proceso']
-  end
-end
-
-puts 'CSV file created:', csv_file
-
-# files_to_process = files.map { |file| directory_path + Shellwords.escape(file[:original_name]) }.join(' ')
-files_to_process = files.map { |file| directory_path + file[:original_name] }
-# puts files_to_process
-
-# create file
-# pdf_images = system("img2pdf #{files_to_process} -o #{directory_path}postulaciones_#{application_month}.pdf")
-pdf_images = system('img2pdf', *files_to_process, '-o',
-                    "#{directory_path}postulaciones_#{application_month}.pdf")
-puts "PDF created: #{pdf_images}"
+files = process_directory(directory_path)
+# puts files
+create_csv(directory_path, files)
+create_pdf(directory_path, files)
